91f81b0303fe03ba83447fcb119981744ff29985
dormaayn <dor.d.ma@gmail.com>
2017-05-18 18:11:25 +0300

    Foundations for the XML Parsing - got the structure

--- Book-316331297-208461871/book-app/src/test/java/BookScoreInitializerTest.java
+++ Book-316331297-208461871/book-app/src/test/java/BookScoreInitializerTest.java
@@ -16,4 +16,16 @@ public class BookScoreInitializerTest {
 
 	}
 
+	@Test
+	public void test1() {
+		BookScoreInitializerImpl init = new BookScoreInitializerImpl();
+		init.setup("<Root>" + "<Reviewer Id=\"123\">" + "<Review>" + "<Id>Foobar</Id>" + "<Score>10</Score>"
+				+ "</Review>" + "<Review>" + "<Id>Moobar</Id>" + "<Score>3</Score>" + "</Review>" + "</Reviewer>"
+				+ "<Reviewer Id=\"678\">" + "<Review>" + "<Id>Boobar</Id>" + "<Score>5</Score>" + " </Review>"
+				+ "<Review>" + "<Id>bush</Id>" + "<Score>9</Score>" + " </Review>" + "<Review>" + " <Id>oop</Id>"
+				+ "<Score>2</Score>" + "</Review>" + "</Reviewer>" + "<Reviewer Id=\"897\">" + "</Reviewer>"
+				+ "</Root>");
+
+	}
+
 }
