ec03634a3f4e94703afd13b0ef1ff31a873f0cc1
Niv Shalmon <shalmon.niv@gmail.com>
2017-05-18 15:57:06 +0300

    findByKeys now uses BinarySearch twice

--- Book-316331297-208461871/library/src/main/java/library/BinarySearch.java
+++ Book-316331297-208461871/library/src/main/java/library/BinarySearch.java
@@ -5,20 +5,31 @@ import java.util.Optional;
 import il.ac.technion.cs.sd.book.ext.LineStorage;
 
 class BinarySearch {
-  static Optional<String> of(LineStorage storer, String key, int low, int high) throws InterruptedException {
-    low = low / 2;
-    high = high / 2 - 1;
-    while (high >= low) {
-      int mid = (low + high) / 2;
-      String current = storer.read(2 * mid);
-      int comparison = current.compareTo(key);
-      if (comparison == 0)
-        return Optional.of(storer.read(2 * mid + 1));
-      if (comparison < 0)
-        low = mid + 1;
-      else
-        high = mid - 1;
-    }
-    return Optional.empty();
-  }
+	/**
+	 * performs an efficient binary search on a {@link LineStorage}
+	 * @param storer the {@link LineStorage}, written in a format of key in one line,
+	 * followed by value in the next, sorted by key
+	 * @param key the key to be searched
+	 * @param low the line number of the first key, 0 for whole file search
+	 * @param high the number of lines used, numberOfLines() for entire file search
+	 * @return an Optional with the value saved for key, or Optional.empty() if the
+	 * key doesn't exist
+	 * @throws InterruptedException if the storer is interrupted
+	 */
+	static Optional<String> of(LineStorage storer, String key, int low, int high) throws InterruptedException {
+		low = low / 2;
+		high = high / 2 - 1;
+		while (high >= low) {
+			int mid = (low + high) / 2;
+			String current = storer.read(2 * mid);
+			int comparison = current.compareTo(key);
+			if (comparison == 0)
+				return Optional.of(storer.read(2 * mid + 1));
+			if (comparison < 0)
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return Optional.empty();
+	}
 }
--- Book-316331297-208461871/library/src/main/java/library/DoubleKeyDict.java
+++ Book-316331297-208461871/library/src/main/java/library/DoubleKeyDict.java
@@ -11,84 +11,88 @@ import il.ac.technion.cs.sd.book.ext.LineStorage;
 import il.ac.technion.cs.sd.book.ext.LineStorageFactory;
 
 public class DoubleKeyDict {
-  private Dict mainKeyMap;
-  private Dict secondaryKeyMap;
-  private LineStorage storer;
-  private boolean initialized;
+	private Dict mainKeyMap;
+	private Dict secondaryKeyMap;
+	private LineStorage storer;
+	private boolean initialized;
 
-  private class IntegerWrapper {
-    public int val;
+	private class IntegerWrapper {
+		public int val;
 
-    public IntegerWrapper() {
-      this.val = 0;
-    }
-    
-    public String toString(){
-      return val+"";
-    }
-  }
+		public IntegerWrapper() {
+			this.val = 0;
+		}
 
-  public DoubleKeyDict(LineStorageFactory factory) {
-    mainKeyMap = new Dict(factory.open("mainKeyMap"));
-    secondaryKeyMap = new Dict(factory.open("secondaryKeyMap"));
-    storer = factory.open("valuesMap");
-    initialized = false;
-  }
-  
-  public void store(Collection<Triple> triples) {
-    assert !initialized;
-    initialized = true;
-    final Map<String, List<Pair>> mainKeyToPair = new HashMap<>();
-    final Map<String, List<Pair>> secondaryKeyToPair = new HashMap<>();
-    triples.stream().forEach(element -> {
-      addToMap(mainKeyToPair, element.getKey1(), new Pair(element.getKey2(), element.getValue()));
-      addToMap(secondaryKeyToPair, element.getKey2(), new Pair(element.getKey1(), element.getValue()));
-    });
-    IntegerWrapper currentLine = new IntegerWrapper();
-    storeDict(mainKeyMap,mainKeyToPair, currentLine);
-    storeDict(secondaryKeyMap,secondaryKeyToPair, currentLine);
-  }
-  
-  private void storeDict(final Dict dict,final Map<String, List<Pair>> m, IntegerWrapper currentLine) {
-    m.keySet().stream().sorted().forEachOrdered(key -> {
-      int startingLine = currentLine.val;
-      m.get(key).stream().sorted().forEachOrdered(element -> {
-        storer.appendLine(element.getKey());
-        storer.appendLine(element.getValue());
-        currentLine.val += 2;
-      });
-      dict.add(new Pair(key,startingLine+","+currentLine));
-    });
-    dict.store();
-  }
- 
-  private void addToMap(final Map<String, List<Pair>> m, String key, Pair value) {
-    if (!m.containsKey(key))
-      m.put(key, new ArrayList<>());
-    m.get(key).add(value);
-  }
-  
-  public List<Pair> findByMainKey(String key) throws InterruptedException{
-    return findByKey(mainKeyMap,key);
-  }
-  
-  public Optional<String> findByKeys(String key1, String key2) throws InterruptedException{
-    Optional<Pair> $ = findByKey(mainKeyMap,key1).stream().filter( p -> p.getKey().equals(key2)).findFirst();
-    return !$.isPresent() ? Optional.empty() : Optional.of($.get().getValue());
-  }
-  
-  public List<Pair> findBySecondaryKey(String key) throws InterruptedException{
-    return findByKey(secondaryKeyMap,key);
-  }
+		public String toString() {
+			return val + "";
+		}
+	}
 
-  private List<Pair> findByKey(Dict d,String key) throws InterruptedException {
-    List<Pair> $ = new ArrayList<>();
-    if(!d.find(key).isPresent())
-      return $;
-    String[] lines = d.find(key).get().split(",");
-    int end = Integer.parseInt(lines[1]);
-    for(int i = Integer.parseInt(lines[0]) ; i<end ;i+=2 )
-      $.add(new Pair(storer.read(i),storer.read(i+1)));
-    return $;
-  }
+	public DoubleKeyDict(LineStorageFactory factory) {
+		mainKeyMap = new Dict(factory.open("mainKeyMap"));
+		secondaryKeyMap = new Dict(factory.open("secondaryKeyMap"));
+		storer = factory.open("valuesMap");
+		initialized = false;
+	}
+
+	public void store(Collection<Triple> triples) {
+		assert !initialized;
+		initialized = true;
+		final Map<String, List<Pair>> mainKeyToPair = new HashMap<>();
+		final Map<String, List<Pair>> secondaryKeyToPair = new HashMap<>();
+		triples.stream().forEach(element -> {
+			addToMap(mainKeyToPair, element.getKey1(), new Pair(element.getKey2(), element.getValue()));
+			addToMap(secondaryKeyToPair, element.getKey2(), new Pair(element.getKey1(), element.getValue()));
+		});
+		IntegerWrapper currentLine = new IntegerWrapper();
+		storeDict(mainKeyMap, mainKeyToPair, currentLine);
+		storeDict(secondaryKeyMap, secondaryKeyToPair, currentLine);
+	}
+
+	private void storeDict(final Dict dict, final Map<String, List<Pair>> m, IntegerWrapper currentLine) {
+		m.keySet().stream().sorted().forEachOrdered(key -> {
+			int startingLine = currentLine.val;
+			m.get(key).stream().sorted().forEachOrdered(element -> {
+				storer.appendLine(element.getKey());
+				storer.appendLine(element.getValue());
+				currentLine.val += 2;
+			});
+			dict.add(new Pair(key, startingLine + "," + currentLine));
+		});
+		dict.store();
+	}
+
+	private void addToMap(final Map<String, List<Pair>> m, String key, Pair value) {
+		if (!m.containsKey(key))
+			m.put(key, new ArrayList<>());
+		m.get(key).add(value);
+	}
+
+	public List<Pair> findByMainKey(String key) throws InterruptedException {
+		return findByKey(mainKeyMap, key);
+	}
+
+	public Optional<String> findByKeys(String key1, String key2) throws InterruptedException {
+		Optional<String> o = mainKeyMap.find(key1);
+		if (!o.isPresent())
+			return Optional.empty();
+		String[] lines = o.get().split(",");
+		return BinarySearch.of(storer, key2 ,Integer.parseInt(lines[0]),Integer.parseInt(lines[1]));
+	}
+
+	public List<Pair> findBySecondaryKey(String key) throws InterruptedException {
+		return findByKey(secondaryKeyMap, key);
+	}
+
+	private List<Pair> findByKey(Dict d, String key) throws InterruptedException {
+		List<Pair> $ = new ArrayList<>();
+		Optional<String> o = d.find(key);
+		if (!o.isPresent())
+			return $;
+		String[] lines = o.get().split(",");
+		int end = Integer.parseInt(lines[1]);
+		for (int i = Integer.parseInt(lines[0]); i < end; i += 2)
+			$.add(new Pair(storer.read(i), storer.read(i + 1)));
+		return $;
+	}
 }
